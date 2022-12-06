9120a4235ac1f429a82376863b6d95480989fe1e
GodfredAsa <goldenheroe1@yahoo.com>
2022-12-06 08:59:00 +0000

    adding files for finding java and bash version

---
+++ subprocess/execute_cmd.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+os.system("cls")
+#  command as a string sample below 
+
+cmd = "ls -lrt"
+sp = subprocess.Popen(cmd, shell= True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+rc = sp.wait()
+out, err = sp.communicate()
+
+# splitlines help return output as list 
+print(f'Return Code: {rc}')
+print(f'Output Code: {out}')
+print(f'Error Code: {err}')
+
+
+
+
+# TAKING COMMAND AS A LIST, APPLY SPLIT ON THE STRING AS A COMMAND
+'''cmd = "echo $HOME".split()
+sp = subprocess.Popen(cmd, shell= False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+rc = sp.wait()
+out, err = sp.communicate()
+
+# splitlines help return output as list  
+print(f'Return Code: {rc}')
+print(f'Output Code: \n{out.splitlines()}')
+print(f'Error Code: \n{err.splitlines()}')
+
+'''
---
+++ subprocess/find_bash_verion.py
@@ -0,0 +1,38 @@
+import subprocess
+import re
+import os
+
+
+def find_bash_version_linux():
+    cmd = "bash --version".split()
+    sp = subprocess.Popen(cmd, shell = False, stdout = subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    rc =  sp.wait()
+
+    out, err = sp.communicate()
+    if rc==0:
+        for each_line in out.splitlines():
+            if 'version' in each_line and 'release' in each_line:
+                value = each_line.split(" ")[3].split('(')[0]
+                print(f'Bash Version: {value}')
+    else:
+        print(f'Error: {err}\n Error occurred')
+        
+        
+
+
+
+def find_java_version():
+    cmd = "java -version".split()
+    sp = subprocess.Popen(cmd, shell = False, stdout = subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    rc =  sp.wait()
+    err, output = sp.communicate()
+    if rc ==0:
+       print(output)
+    else:
+        print(f'Output: {err}')
+    
+
+find_bash_version_linux()
+print("<==----------------------------------------------------------------------------------==>")
+find_java_version()
+print("<==----------------------------------------------------------------------------------==>")
