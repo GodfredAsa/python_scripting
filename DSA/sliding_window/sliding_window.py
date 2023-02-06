34057590edd99f4118d74c3785a7e9cb4885cec2
GodfredAsa <goldenheroe1@yahoo.com>
2023-02-06 02:53:47 +0000

    minor changes

--- DSA/sliding_window/sliding_window.py
+++ DSA/sliding_window/sliding_window.py
@@ -8,7 +8,8 @@ import unittest
 Difference between the maximum and minimum average of all k-length continuous subarrays
 Input: arr[ ] = {3, 8, 9, 15}, K = 2
 Output: 6.5
-Explanation: All subarrays of length 2 are {3, 8}, {8, 9}, {9, 15} and their averages are (3+8)/2 = 5.5, (8+9)/2 = 8.5, and (9+15)/2 = 12.0 respectively.
+Explanation: All subarrays of length 2 are {3, 8}, {8, 9}, {9, 15} 
+and their averages are (3+8)/2 = 5.5, (8+9)/2 = 8.5, and (9+15)/2 = 12.0 respectively.
 
 Therefore, the difference between the maximum(=12.0) and minimum(=5.5) is 12.0 -5.5 = 6.5.
 
